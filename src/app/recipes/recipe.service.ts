c8087dca1fda6440c339369742dad95f0cd2d03b
Yves Jaques <ymoraisjaques@gmail.com>
2024-06-09 15:55:44 -0300

    Adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe-list/recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>()
 
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'This is simply a test', 'https://bakesbybrownsugar.com/wp-content/uploads/2021/10/Apple-Cakes-with-Pistachios-.jpg'),
-        new Recipe('Test Recipe 2', 'This is simply a test 2', 'https://bakesbybrownsugar.com/wp-content/uploads/2021/10/Apple-Cakes-with-Pistachios-.jpg')
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super Tasty Schnitzel - just awesome',
+            'https://assets.bonappetit.com/photos/57ae1afd53e63daf11a4e26f/1:1/w_1920,c_limit/chicken-schnitzel.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Big fat burger',
+            'A big fat burger',
+            'https://c1.peakpx.com/wallpaper/791/638/395/appetite-beef-big-bread-bun-burger-wallpaper-preview.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1),
+            ])
     ]
 
     getRecipes() {
